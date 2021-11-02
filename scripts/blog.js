54ef9cc9944644814e94faa3d364919a1651c1b6
bread2077 <90520286+bread2077@users.noreply.github.com>
2021-11-02 14:52:25 +0300

    Delete scripts directory

--- scripts/blog.js
+++
@@ -1,9 +0,0 @@
-
-function nextfun(id,eleppent){
-	for (let i = 0; i < document.getElementsByClassName('block__hidden').length; i++) {
-		document.getElementsByClassName('block__hidden')[i].style.display = 'none';
-		document.getElementsByClassName('numbers_animate')[i].classList.remove("numbers_animate_active")
-	}
-	document.getElementById(id).style.display = 'block';
-	eleppent.classList.add("numbers_animate_active")
-}
