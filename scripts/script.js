8a767683f0b0108299c0301a2723eaf3aba8381f
Anatoliy Chudnov <tolik.chudnov@gmail.com>
2023-06-10 20:26:55 +0300

    feat: сделать раздел faq

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,34 @@ const firstCulc = new ChoiceService('first-culc');
 
 firstCulc.renderSocials();
 firstCulc.setEventListeners();
+
+class Faq {
+  constructor(faqBlock) {
+    this._faqBlock = faqBlock
+    this._descriptionBlock = this._faqBlock.querySelector('.faq__description');
+    this._announceBlock = this._faqBlock.querySelector('.faq__announce');
+    this._descriptionText = this._faqBlock.querySelector('.faq__text');
+    this._button = this._faqBlock.querySelector('.faq__button');
+  }
+
+  _toggleDescription() {
+    this._descriptionBlock.classList.toggle('faq__description_closed');
+    this._descriptionText.classList.toggle('faq__text_closed');
+    this._announceBlock.classList.toggle('faq__announce_opened');
+  }
+
+  setEventListeners() {
+    this._button.addEventListener('click', () => {
+      this._toggleDescription();
+      this._button.classList.toggle('faq__button_type_close');
+    });
+  }
+
+}
+
+const faqList = document.querySelectorAll('.faq__item');
+
+faqList.forEach(faq => {
+  const faqItem = new Faq(faq);
+  faqItem.setEventListeners();
+});
