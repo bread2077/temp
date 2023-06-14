9067bd53db5346b12651fd65971a69760e68b343
Anatoliy Chudnov <tolik.chudnov@gmail.com>
2023-06-15 00:19:19 +0300

    refactor: добавить класс HorizontalSwiper

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 import ChoiceService from './ChoiceService.js';
 import Faq from './Faq.js';
-
+import HorizontalSwiper from './HorizontalSwiper.js';
 
 const firstCulc = new ChoiceService('first-culc');
 firstCulc.renderSocials();
@@ -12,47 +12,6 @@ faqList.forEach(faq => {
   faqItem.setEventListeners();
 });
 
-const phonesBlock = document.querySelector('.advantages__phone-list');
-const phonesList = document.querySelectorAll('.advantages__item_type_phone');
-
-console.log(phonesList);
-
-const touchCoord = {
-  x: 0
-}
-
-function handleTouchStart(e) {
-  touchCoord.x = e.targetTouches[0].clientX;
-}
-
-function handleTouchMove(e) {
-  const xUp = e.targetTouches[0].clientX;
-  const { x } = touchCoord;
-
-  const DETECT_TRESHHOLD = 100;
-
-  if(Math.abs(xUp - x) > DETECT_TRESHHOLD) {
-
-    if(xUp > x) {
-      phonesBlock.style.transform = `translate(${width}px)`;
-      phonesList[0].classList.add('advantages__item_active');
-      phonesList[1].classList.remove('advantages__item_active');
-    } else {
-      phonesBlock.style.transform = `translate(-${width}px)`;
-      phonesList[1].classList.add('advantages__item_active');
-      phonesList[0].classList.remove('advantages__item_active');
-    }
-
-  }
-}
-
-phonesBlock.addEventListener('touchstart', (e) => handleTouchStart(e));
-phonesBlock.addEventListener('touchmove', (e) => handleTouchMove(e));
-
-console.log(window.screen.width);
-console.log(phonesList[0].offsetWidth);
-console.log(window.screen.width - phonesList[0].offsetWidth);
-const width = phonesList[0].offsetWidth / 2;
-
-phonesBlock.style.transform = `translate(${width}px)`;
+const phoneSwiper = new HorizontalSwiper('.advantages__phone-list');
+phoneSwiper.initializeSwiper();
 
