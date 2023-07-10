6040498286814f6c125d04f44f0c67104a431d52
Anatoliy Chudnov <tolik.chudnov@gmail.com>
2023-07-10 23:48:02 +0300

    feat: rename chunks

--- scripts/base.v8.js
+++ scripts/base.v8.js
@@ -3,42 +3,45 @@ import '../pages/global.v17.css';
 import ListOpener from './components/ListOpener.js';
 import {
   menuBurger,
-  menuBurgerSpans,
-  headerMenu,
   root,
   upperFooterLinks,
   dateElement,
   header,
-  signinButton,
 } from './utils/constants.v5.js';
 
-function toggleHeaderMenu() {
-  root.classList.toggle('root_hidden');
-  header.classList.toggle('header__mobile_opened');
-  headerMenu.classList.toggle('header__nav_opened');
-  menuBurgerSpans.forEach(item => {
-    item.classList.toggle('menu-burger__item_active');
-  });
-}
+if (menuBurger) {
+  const menuBurgerSpans = menuBurger.querySelectorAll('.menu-burger__item');
+  const headerMenu = header.querySelector('.header__nav');
+  const signinButton = header.querySelector('.header__button_role_signin');
 
-function closeHeaderMenu() {
-  root.classList.remove('root_hidden');
-  header.classList.remove('header__mobile_opened');
-  headerMenu.classList.remove('header__nav_opened');
-  menuBurgerSpans.forEach(item => {
-    item.classList.remove('menu-burger__item_active');
-  });
-}
+  function toggleHeaderMenu() {
+    root.classList.toggle('root_hidden');
+    header.classList.toggle('header__mobile_opened');
+    headerMenu.classList.toggle('header__nav_opened');
+    menuBurgerSpans.forEach(item => {
+      item.classList.toggle('menu-burger__item_active');
+    });
+  }
 
-menuBurger.addEventListener('click', toggleHeaderMenu);
-signinButton.addEventListener('click', closeHeaderMenu);
+  function closeHeaderMenu() {
+    root.classList.remove('root_hidden');
+    header.classList.remove('header__mobile_opened');
+    headerMenu.classList.remove('header__nav_opened');
+    menuBurgerSpans.forEach(item => {
+      item.classList.remove('menu-burger__item_active');
+    });
+  }
 
-if (document.documentElement.clientWidth < 980) {
-  const mainMenu = new ListOpener(
-    '.header__links-list',
-    '.header__link_type_menu'
-  );
-  mainMenu.initializeOpener();
+  menuBurger.addEventListener('click', toggleHeaderMenu);
+  signinButton.addEventListener('click', closeHeaderMenu);
+
+  if (document.documentElement.clientWidth < 980) {
+    const mainMenu = new ListOpener(
+      '.header__links-list',
+      '.header__link_type_menu'
+    );
+    mainMenu.initializeOpener();
+  }
 }
 
 const footerMenu = new ListOpener('.footer', '.footer__link_type_menu');
