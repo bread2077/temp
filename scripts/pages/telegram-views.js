ed67d0556ea20484106584add093e2b83c0ca302
Anatoliy Chudnov <tolik.chudnov@gmail.com>
2023-07-22 16:48:41 +0300

    feat: add select filter

--- scripts/pages/telegram-views.js
+++ scripts/pages/telegram-views.js
@@ -40,16 +40,69 @@ countersList.forEach(counter => {
 const popupService = new PopupWithDescription('popup_type_service');
 popupService.setEventListeners();
 
-socials.telegram.services.views.full.forEach(social => {
+const socialsName = document
+  .querySelector('[data-socials]')
+  .getAttribute('data-socials')
+  .split('.');
+
+const [socialTitle, socialServices] = socialsName;
+
+const setServicesItem = item => {
   const serviceItem = new FullService(
     'service-list__body',
     'service-element',
     {
-      servicesItem: social,
+      servicesItem: item,
     },
     popupService.openServicePopup.bind(popupService)
   );
 
   serviceItem.setEvetListeners();
   listContainer.append(serviceItem.createServiceItem());
-});
+};
+const setServicesItems = serviceItems => {
+  serviceItems.forEach(social => {
+    setServicesItem(social);
+  });
+};
+
+const serviceSelectForm = document.querySelector('.service-list__form');
+
+if (socialsName.length > 1) {
+  setServicesItems(socials[socialTitle].services[socialServices].full);
+  serviceSelectForm.classList.add('service-list__form_hided');
+} else {
+  serviceSelectForm.classList.remove('service-list__form_hided');
+  setServicesItems(socials[socialTitle].shortList.full);
+  const serviceSelect = document.querySelector(
+    '.service-list__input_type_select'
+  );
+
+  const serviceSelectElement = document
+    .getElementById('service-select')
+    .content.querySelector('.service-list__option');
+  console.log(serviceSelectElement);
+
+  const typesArray = Array.from(socials[socialTitle].shortList.full).map(
+    type => type.type
+  );
+
+  const uniqueTypesArray = new Set(typesArray);
+
+  uniqueTypesArray.forEach(type => {
+    const [title, value] = type.split(' ');
+    const select = serviceSelectElement.cloneNode(true);
+    select.textContent = title;
+    select.value = value;
+
+    serviceSelect.append(select);
+  });
+
+  serviceSelect.addEventListener('input', e => {
+    listContainer.innerHTML = '';
+    const filterArray = socials[socialTitle].shortList.full.filter(
+      item => item.type.endsWith(e.target.value) && item
+    );
+    setServicesItems(filterArray);
+  });
+}
