eae3e3d167dc4c93ef19de7c7d9208ffc3984e29
Анатолий Чуднов <60097191+12-web@users.noreply.github.com>
2023-12-01 12:14:04 +0300

    Update and rename service-popup.v5.js to service-popup.v6.js

--- scripts/service-popup.v6.js
+++ scripts/service-popup.v6.js
@@ -1,12 +1,12 @@
 const openServicePopup = id => {
   const servicePopup = document.getElementById("popup" + id);
   servicePopup.classList.add("message__show")
-  document.body.style = "overflow: hidden;"
+  document.body.style.setProperty('overflow', 'hidden');
 }
 
 const closeServicePopup = id => {
   const servicePopup = document.getElementById("popup" + id);
   
   servicePopup.classList.remove("message__show")
-  document.body.style = "overflow: visible;"
+  document.body.style.removeProperty('overflow');
 }
